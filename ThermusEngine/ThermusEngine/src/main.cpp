87ba78a53982d49a3e512860f2c7365f34c04f20
Nicholas <Nicholasbaker609@gmail.com>
2025-02-04 22:12:00 -0500

    Initial Commit

---
+++ ThermusEngine/ThermusEngine/src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+//#include <chrono>
+//#include <thread>
+#include <raylib.h>
+#include <array>
+#include <vector>
+
+void GameLogic(int lastKeyPressed){
+        
+}
+void Paused(){
+    
+}
+void GameOver(){
+    
+}
+// Method where we, well render. Using game logic updated every timestep, it renders each tile deciding its' color based on if the tile is a snake, food, or normal tile.
+void Render(){
+        BeginDrawing();
+            ClearBackground(DARKPURPLE);
+        EndDrawing();
+}
+
+int main() 
+{   
+    InitWindow(800, 800, "Thermus Application");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        Render();
+    }
+    
+    CloseWindow();
+    return 0;
+}
---
+++ ThermusEngine/build/CMakeFiles/Thermus_Application.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Thermus_Application.dir/media/nicholas/E/ThermusEngine/Thermus-Engine/src/main.cpp.o"
+  "CMakeFiles/Thermus_Application.dir/media/nicholas/E/ThermusEngine/Thermus-Engine/src/main.cpp.o.d"
+  "Thermus_Application"
+  "Thermus_Application.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Thermus_Application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
