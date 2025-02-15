99fa3592fb8a1f52d39ca4a977337d7d56db36c4
Nicholas <Nicholasbaker609@gmail.com>
2025-02-15 18:02:54 -0500

    test

--- ThermusEngine/TowerDefenseGame/src/Tile.cpp
+++
@@ -1,7 +0,0 @@
-#include "Tile.h"
-
-void Tile::SetScale(float i){
-    //Scale = i;
-    texture.height *= i;
-    texture.width *= i;
-}
--- ThermusEngine/TowerDefenseGame/src/include.h
+++
@@ -1,7 +0,0 @@
-// ALL ROOT LEVEL INCLUDES
-#include <iostream>
-#include <raylib.h>
-#include <array>
-#include <vector>
-#include <functional>
-#include <string>
